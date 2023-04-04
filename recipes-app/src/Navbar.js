dc3d97fd49133d0a930eba4ddbf4634c1af9c7a4
unknown <ramanasiddartha390@gmail.com>
2023-04-05 00:16:46 +0530

    Signed-off-by: unknown <ramanasiddartha390@gmail.com>

---
+++ recipes-app/src/Navbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand-sm navbar-light bg-light"></nav>
+      </div>
+    )
+  }
+}
